080a4bb4771df65b9af0efc5c3da2cb4245a2502
A-Shariatix <1384asha1384@gmail.com>
2025-01-01 14:52:06 +0330

    Update Server.py

--- Server.py
+++ Server.py
@@ -48,6 +48,12 @@ database or not.
 :param data: stores the data sent by user
 :type data: json
 
+:param username: stores json data's key in a list
+:type username: list
+
+:param password: stores json data's value in a hashed format
+:type password: bytes
+
 :return: returns 400 status code and a message if data already exists
 in database. otherwise a 204 status code and a different message will
 be returned.
@@ -67,11 +73,23 @@ def login():
     return lang.User_Data_Input_Invalid, 400
 
 
-""" forgotten_password code: first gets data in json format from user,
- including username and password; then checks if both username 
- and password do exist. after that, it hashes the password and
- checks if the hashed password and the username exist in database.
- if it exists, the query will be executed."""
+"""this is a decorator function which firstly checks if user's
+input structure is valid. then checks if user's data exists in
+database or not. then tries to update password in database.
+
+:param data: stores the data sent by user
+:type data: json
+
+:param username: stores json data's key in a list
+:type username: list
+
+:param password: stores json data's value in a hashed format
+:type password: bytes
+
+:return: returns 400 status code and a message if data is invalid
+otherwise a 204 status code and a different message will be returned
+:rtype: string, http status code
+"""
 
 
 @app.route('/forgotten_password', methods=['POST'])
