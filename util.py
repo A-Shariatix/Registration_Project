81abfef04084337442c08a1692c4bc56d570bcdd
A-Shariatix <1384asha1384@gmail.com>
2025-04-20 22:32:56 +0330

    Update util.py

--- util.py
+++ util.py
@@ -2,71 +2,66 @@ import bcrypt
 
 
 def check_user_credentials(username, password, cursor):
-"""this function checks if user's information including
-username and password exists in database.
+    """
+    This function checks if user's information including
+    username and password exist in database.
 
-:param username: stores json data's key in a list
-:type username: list
+    :param username: First item of the generated list, which is made of received json data
+    :type username: str
 
-:param password: stores json data's value in a hashed format
-:type password: bytes
+    :param password: The value of the only key in received json data, which gets hashed by generate_hash function
+    :type password: bytes
 
-:param cursor: stores the connected cursor to database
-:type cursor: Cursor
+    :param cursor: A database object that let us execute queries row by row
+    :type cursor: MySQLCursor
 
-:param user_exist: stores the number of infected rows
-:type user_exist: int
+    :return: True if matching credentials exist in database, otherwise false
+    :rtype: bool
+    """
+    user_exist = cursor.execute('''select username, password
+                                from information where username = %s
+                                and password = %s''', (username, password))
 
-:return: True if both expected username and password
-exist in database. otherwise False.
-:rtype: bool
-"""
-    user_exist = cursor.execute(f'select username, password '
-                                f'from information where username = "{username}"'
-                                f' and password = "{password}"')
+    fetcher = cursor.fetchone()
+    print(fetcher)
+    print(f'matching users found : {user_exist}')
     if user_exist == 1:
         return True
-    else:
+    elif user_exist == 0:
         return False
 
 
-def check_input_data(data, username):
-"""this function checks validation of user's input.
+def check_input_data(user_data, username):
+    """
+    This function checks if client has entered username and password or not
 
-:param data: stores user's sent data in json format
-:type data: json
+    :param user_data: Json format data sent by client, including username and password
+    :type user_data: dict
 
-:param username: stores data's key
-:type username: string
+    :param username: First item of the generated list, which is made of received json data
+    :type username: str
 
-:return: True if username and password are entered;
-otherwise False.
-:rtype: bool
-"""
-    if len(username) > 0 and len(data[username]) > 0:
+    :return: True if username and password exists in received json data, otherwise false
+    :rtype: bool
+    """
+    if len(username) > 0 and len(user_data[username]) > 0:
         return True
     return False
 
 
-def generate_hash(data, username):
-"""this function hashes the given stringand returns
-it as hashed password.
+def generate_hash(user_data, username):
+    """
+    This function generates a hashed password using salt, and it encodes using utf-8
 
-:param data: stores user's sent data in json format
-:type data: json
+    :param user_data: Json format data sent by client, including username and password
+    :type user_data: dict
 
-:param username: stores info's key
-:type username: string
+    :param username: First item of the generated list, which is made of received json data
+    :type username: str
 
-:param salt: stores a pre ordered salt
-:type salt: bytes
-
-:param password: stores json data's value in a hashed format
-:type password: bytes
-
-:return: returns hashed password
-:rtype: bytes
-"""
+    :return: hashed password
+    :rtype: bytes
+    """
     salt = b'$2b$12$NmjU6/raMmtCvJMHgx6ht.'
-    password = bcrypt.hashpw(data[username].encode("utf-8"), salt)
+    password = bcrypt.hashpw(user_data[username].encode("utf-8"), salt)
     return password
