45a0e28eb3b8554ccabb782f75d375d1f4f45407
A-Shariatix <1384asha1384@gmail.com>
2025-04-21 19:42:11 +0330

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
 import pymysql
+from dbutils import pooled_db
+from flask import Flask, request, g
 import lang_en as lang
 from util import check_user_credentials, generate_hash, check_input_data
 
@@ -7,105 +8,102 @@ from util import check_user_credentials, generate_hash, check_input_data
 app = Flask(__name__)
 
 
+app.pool = pooled_db.PooledDB(
+        creator=pymysql,
+        database="users",
+        host="database",
+        port=3306,
+        user="batman",
+        password="root",
+        maxconnections=4,
+        blocking=True
+    )
+
+
+@app.before_request
+def setup_conn():
+    g.conn = app.pool.connection()
+
+
+@app.teardown_request
+def close_conn(exception):
+    if hasattr(g, "cursor"):
+        g.cursor.close()
+    if hasattr(g, "conn"):
+        g.conn.close()
+
+
 @app.route('/sign_up', methods=['POST'])
 def sign_up():
-"""this is a decorator function which firstly checks if user's
-input structure is valid. then checks if user's data exists in
-database or not. then tries to insert entered data into database.
+    """
+    this is a decorator function which firstly checks if user's
+    input structure is valid. then checks if user's data exists in
+    database or not. then tries to insert entered data into database.
 
-:param data: stores the data sent by user
-:type data: json
-
-:param username: stores json data's key by using list
-:type username: string
-
-:param password: stores json data's value in a hashed format
-:type password: bytes
-
-:return: returns 400 status code and a message if data is invalid
-otherwise a 204 status code and a different message will be returned
-:rtype: string, http status code
-"""
+    :return: returns 400 status code and a message if data is invalid.
+    otherwise a 200 status code and a different message will be returned
+    :rtype: string, http status code
+    """
     data = request.get_json()
     username = list(data)[0]
     if check_input_data(data, username) is True:
         password = generate_hash(data, username)
-        if check_user_credentials(username, password, cursor) is False:
-            cursor.execute(f'insert into information (username, password) values ("{username}", "{password}")')
-            conn.commit()
-            return lang.Signup_success, 204
-        else:
-            return lang.Username_unavailable, 400
+        with g.conn.cursor() as cursor:
+            if check_user_credentials(username, password, cursor) is False:
+                cursor.execute('insert into information (username, password) values (%s, %s)', (username, password))
+                g.conn.commit()
+                return lang.Signup_success, 200
+            else:
+                return lang.Username_unavailable, 400
     return lang.User_Data_Input_Invalid, 400
 
 
 @app.route('/login', methods=['POST'])
 def login():
-"""this is a decorator function which firstly checks if user's
-input structure is valid. then checks if user's data exists in
-database or not.
+    """
+    this is a decorator function which firstly checks if user's
+    input structure is valid. then checks if user's data exists in
+    database or not.
 
-:param data: stores the data sent by user
-:type data: json
-
-:param username: stores json data's key by using list
-:type username: string
-
-:param password: stores json data's value in a hashed format
-:type password: bytes
-
-:return: returns 400 status code and a message if data already exists
-in database. otherwise a 204 status code and a different message will
-be returned.
-:rtype: string, http status code"""   
+    :return: returns 400 status code and a message if data already exists
+    in database. otherwise a 200 status code and a different message will
+    be returned.
+    :rtype: string, http status code
+    """
     data = request.get_json()
     username = list(data)[0]
     password = generate_hash(data, username)
     if check_input_data(data, username) is True:
-        if check_user_credentials(username, password, cursor) is True:
-            return lang.Login_success, 204
-        else:
-            return lang.User_Data_Input_Invalid, 400
+        with g.conn.cursor() as cursor:
+            if check_user_credentials(username, password, cursor) is True:
+                return lang.Login_success, 200
+            else:
+                return lang.User_Data_Input_Invalid, 400
     return lang.User_Data_Input_Invalid, 400
 
 
 @app.route('/forgotten_password', methods=['POST'])
 def forgotten_password():
-"""this is a decorator function which firstly checks if user's
-input structure is valid. then checks if user's data exists in
-database or not. then tries to update password in database.
+    """
+    this is a decorator function which firstly checks if user's
+    input structure is valid. then checks if user's data exists in
+    database or not. then tries to update password in database.
 
-:param data: stores the data sent by user
-:type data: json
-
-:param username: stores json data's key by using list
-:type username: string
-
-:param password: stores json data's value in a hashed format
-:type password: bytes
-
-:return: returns 400 status code and a message if data is invalid
-otherwise a 204 status code and a different message will be returned
-:rtype: string, http status code
-"""
+    :return: returns 400 status code and a message if data is invalid
+    otherwise a 200 status code and a different message will be returned
+    :rtype: string, http status code
+    """
     data = request.get_json()
     username = list(data)[0]
     if check_input_data(data, username) is True:
         password = generate_hash(data, username)
-        if check_user_credentials(username, password, cursor) is True:
-            cursor.execute(f'update information set password = "{password}" where username = "{username}"')
-            conn.commit()
-            return lang.Update_password, 204
+        with g.conn.cursor() as cursor:
+            if check_user_credentials(username, password, cursor) is True:
+                cursor.execute('update information set password = %s where username = %s', (password, username))
+                g.conn.commit()
+                return lang.Update_password, 200
     return lang.User_Data_Input_Invalid, 400
 
 
 if __name__ == '__main__':
-    conn = pymysql.connect(
-        host='localhost',
-        password='root',
-        database='users',
-        port=3306,
-        user='root'
-    )
-    cursor = conn.cursor()
-    app.run()
+    app.run(host="0.0.0.0", port=7070)
